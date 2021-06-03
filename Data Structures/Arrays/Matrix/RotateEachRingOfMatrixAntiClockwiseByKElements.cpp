e5412e35cac1f2932d32129bf32afb79a854d3d1
hashish_y <77440642+Hashish-y@users.noreply.github.com>
2021-06-03 10:31:32 +0530

    Add files via upload

---
+++ Data Structures/Arrays/Matrix/RotateEachRingOfMatrixAntiClockwiseByKElements.cpp
@@ -0,0 +1,11 @@
+/*C++ preogram to rotate each ring of matrix anticlockwise by k elements 
+Input : k = 3
+        mat[4][4] = {{1, 2, 3, 4},
+                    {5, 6, 7, 8},
+                    {9, 10, 11, 12},
+                    {13, 14, 15, 16}}
+Output: 4 8  12 16
+        3 10  6 15
+        2 11  7 14
+        1  5  9 13   */
+
